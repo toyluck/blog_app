344cd1971e14662d3f2d09c3f63815ee86a53c7e
Administrator <13480020053@163.com>
2016-04-05 18:14:48 +0800

    ............

---
+++ app/src/main/java/com/wugang/src/blog/MainActivity.java
@@ -0,0 +1,56 @@
+package com.wugang.src.blog;
+
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    //声明相关变量
+    private Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private AnimationDrawable mAnimationDrawable;
+    private ImageView ivRunningMan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews(); //获取控件
+        //京东RunningMan动画效果，和本次Toolbar无关
+//        mAnimationDrawable = (AnimationDrawable) ivRunningMan.getBackground();
+//        mAnimationDrawable.start();
+        toolbar.setTitle("Toolbar");//设置Toolbar标题
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff")); //设置标题颜色
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //创建返回键，并实现打开关/闭监听
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+//                mAnimationDrawable.stop();
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+//                mAnimationDrawable.start();
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+    private void findViews() {
+        ivRunningMan = (ImageView) findViewById(R.id.iv_main);
+        toolbar = (Toolbar) findViewById(R.id.tl_custom);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_main_drawer);
+//        lvLeftMenu = (ListView) findViewById(R.id.lv_left_menu);
+    }
+}
